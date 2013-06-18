4b0473c415a426a8aa91449a50453289e67eb038
jchrisman12 <jchrisman12@gmail.com>
2013-06-18 18:50:25 -0500

    Now trying fetching

--- FirstProject/Test.py
+++ FirstProject/Test.py
@@ -5,4 +5,5 @@ print 'Card Value is %d\nCard Suit is %d\nAlso known as the %s of %s'\
     % (card1.getValue(), card1.getSuit(), card1.getValueName(),\
     card1.getSuitName())
     
-print 'Poop'
+    
+#     sdlfjlska
